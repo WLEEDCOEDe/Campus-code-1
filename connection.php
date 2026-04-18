96767d6d3f4bbb2cc53e0218e7c29da21fb2cf21
WLEEDCOEDe <443041344@stu.bu.edu.sa>
2026-04-19 02:23:23 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,12 @@
 <?php
 // هذا الملف يربط موقعنا بقاعدة البيانات
-$conn = mysqli_connect("localhost", "root", "", "my_project");
+$conn = mysqli_connect(
+    getenv('MYSQLHOST')     ?: 'mysql.railway.internal',
+    getenv('MYSQLUSER')     ?: 'root',
+    getenv('MYSQLPASSWORD') ?: 'uVkNBVnuhsmWBozSenWmEOcxzJRQdlhf',
+    getenv('MYSQLDATABASE') ?: 'railway',
+    getenv('MYSQLPORT')     ?: '3306'
+);
 
 if (!$conn) {
     echo "يوجد مشكلة في الاتصال بقاعدة البيانات";
